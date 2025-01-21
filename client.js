06df9792762f65a5b435785554a2836778c30562
GEEKMDXINC <herukobot67@gmail.com>
2025-01-21 19:11:53 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,31 +1,13 @@
-const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const sock = makeWASocket({
+    logger: P({ level: 'silent' }),
+        printQRInTerminal: false,
+        browser: [ "Ubuntu", "Chrome", "20.0.04" ],
+        syncFullHistory: true,
+        generateHighQualityLinkPreview: true,
+        auth: {
+            creds: state.creds,
+            keys: makeCacheableSignalKeyStore(state.keys, P({ level: "fatal" }).child({ level: "fatal" }))
+        },
 
-// Création d'un client WhatsApp
-const client = new Client();
 
-// Événement de connexion
-client.on('qr', (qr) => {
-    // Génération du code QR
-    qrcode.generate(qr, { small: true });
-});
-
-// Événement de connexion réussie
-client.on('ready', () => {
-    console.log('Bot connecté à WhatsApp');
-});
-
-// Événement de réception de message
-client.on('message', async (msg) => {
-    // Vérification du numéro de téléphone de l'émetteur
-    if (msg.from.includes('@c.us')) {
-        // Traitement du message reçu
-        console.log(`Nouveau message de ${msg.from}: ${msg.body}`);
-        
-        // Réponse automatique
-        await client.sendMessage(msg.from, 'Bonjour ! Je suis le bot Ruben-MD. Comment puis-je vous aider ?');
-    }
-});
-
-// Démarrage du client WhatsApp
-client.initialize();
+    sock.ev.on('creds.update', saveCreds);
