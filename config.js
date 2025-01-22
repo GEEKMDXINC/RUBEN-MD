1b562cca4e8d1857eb95eada7abf798b2968a424
GEEKMDXINC <herukobot67@gmail.com>
2025-01-22 14:42:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ if (fs.existsSync('config.env')) {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || 'Ruben-MD',
 OWNER: process.env.OWNER_NUMBER || '237693755398',  
- prefix: process.env.PREFIX || '.',
+ PREFIX: process.env.PREFIX || '.',
    MODE: process.env.MODE || 'prive',
 };
