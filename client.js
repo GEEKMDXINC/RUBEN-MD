2aba1c3a33a3b71a6b235243a5eb0d3907265f2d
GEEKMDXINC <herukobot67@gmail.com>
2025-01-22 16:55:49 +0100

    Update client.js

--- client.js
+++ client.js
@@ -2,13 +2,14 @@ const axios = require("axios");
 const fs = require("fs");
 const pino = require("pino");
 const path = require('path');
-const { default: makeWASocket, useMultiFileAuthState, logger, delay, makeCacheableSignalKeyStore } = require("ovl_wa_baileys");
+const { makeWASocket, useMultiFileAuthState, logger, delay, makeCacheableSignalKeyStore } = require("ovl_wa_baileys");
 
+const credsPath = path.join(__dirname, 'auth');
 
 async function slgAuth() {
     if (!config.SESSION_ID) {
-        console.log('veillez ajouté une session id dans votre config');
-     process.exit(1);
+        console.log('Veuillez ajouter une session id dans votre config');
+        process.exit(1);
     }
     const sessdata = config.SESSION_ID.split("SLG-MD~")[1];
     const url = `https://pastebin.com/raw/${sessdata}`;
@@ -18,26 +19,30 @@ async function slgAuth() {
         await fs.promises.writeFile(credsPath, data);
         console.log("🔒 Session téléchargée avec succès !!");
     } catch (error) {
-        console.error('erreur lors de la récupération de la session id sur pastebin:', error);
+        console.error('Erreur lors de la récupération de la session id sur pastebin:', error);
         process.exit(1);
     }
 }
 
 async function main() {
+    await slgAuth();
 
- slgAuth();
-
-  const { state, saveCreds } = await useMultiFileAuthState(path.join(__dirname, 'auth'));
-        const slg = makeWASocket({
-            printQRInTerminal: true,
-            logger: pino({ level: "silent" }),
-            browser: [ "Ubuntu", "Chrome", "20.0.04" ],
-            generateHighQualityLinkPreview: true,
-            syncFullHistory: false,
-            auth: {
+    const { state, saveCreds } = await useMultiFileAuthState(credsPath);
+    const slg = makeWASocket({
+        printQRInTerminal: true,
+        logger: pino({ level: "silent" }),
+        browser: [ "Ubuntu", "Chrome", "20.0.04" ],
+        generateHighQualityLinkPreview: true,
+        syncFullHistory: false,
+        auth: {
             creds: state.creds,
             keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "silent" }).child({ level: "silent" }))
-        },
-    slg.ev.on('creds.update', saveCreds)
+        }
+    });
 
-main(),
+    slg.ev.on('creds.update', saveCreds);
+
+    // Ajouter d'autres fonctionnalités ici
+}
+
+main();
