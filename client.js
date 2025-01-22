3a4359c13a8e05a91245eae4e0b2f9d44b9ce490
GEEKMDXINC <herukobot67@gmail.com>
2025-01-22 14:28:23 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,13 +1,25 @@
-const sock = makeWASocket({
-    logger: P({ level: 'silent' }),
-        printQRInTerminal: false,
-        browser: [ "Ubuntu", "Chrome", "20.0.04" ],
-        syncFullHistory: true,
-        generateHighQualityLinkPreview: true,
-        auth: {
+
+const pino = require("pino");
+const path = require('path');
+const { default: makeWASocket, useMultiFileAuthState, logger, delay, makeCacheableSignalKeyStore } = require("ovl_wa_baileys");
+
+async function startSlg() {
+  
+    const { state, saveCreds } = await useMultiFileAuthState(path.join(__dirname, 'auth'));
+        try {
+        const slg = makeWASocket({
+            printQRInTerminal: true,
+            logger: pino({ level: "silent" }),
+            browser: [ "Ubuntu", "Chrome", "20.0.04" ],
+            generateHighQualityLinkPreview: true,
+            syncFullHistory: false,
+            auth: {
             creds: state.creds,
-            keys: makeCacheableSignalKeyStore(state.keys, P({ level: "fatal" }).child({ level: "fatal" }))
+            keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "silent" }).child({ level: "silent" }))
         },
 
 
-    sock.ev.on('creds.update', saveCreds);
+    slg.ev.on('creds.update', saveCreds);
+
+startSlg(),
+
