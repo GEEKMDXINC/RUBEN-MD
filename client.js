8676a10a97b2e467ad53d0e2e8ae57f42dc2a9a7
GEEKMDXINC <herukobot67@gmail.com>
2025-01-22 16:09:51 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,9 +1,32 @@
-
+const axios = require("axios");
+const config = require("./config");
 const pino = require("pino");
 const path = require('path');
 const { default: makeWASocket, useMultiFileAuthState, logger, delay, makeCacheableSignalKeyStore } = require("ovl_wa_baileys");
 
+
+async function slgAuth() {
+    if (!config.SESSION_ID) {
+        console.log('veillez ajouté une session id dans votre config');
+     process.exit(1);
+    }
+    const sessdata = config.SESSION_ID.split("SLG-MD~")[1];
+    const url = `https://pastebin.com/raw/${sessdata}`;
+    try {
+        const response = await axios.get(url);
+        const data = typeof response.data === 'string' ? response.data : JSON.stringify(response.data);
+        await fs.promises.writeFile(credsPath, data);
+        console.log("🔒 Session téléchargée avec succès !!");
+    } catch (error) {
+        console.error('erreur lors de la récupération de la session id sur pastebin:', error);
+        process.exit(1);
+    }
+}
+
 async function startSlg() {
+
+ slgAuth();
+
   const { state, saveCreds } = await useMultiFileAuthState(path.join(__dirname, 'auth'));
         const slg = makeWASocket({
             printQRInTerminal: true,
