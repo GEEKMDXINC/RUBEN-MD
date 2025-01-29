754526654365f818d0a768d20335c2c8926d046a
GEEKMDXINC <herukobot67@gmail.com>
2025-01-29 16:21:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ let devss = ['237693755398', '237621713181'];
 
 const devss_id = devss.map(v => v.replace(/[^0-9]/g, '') + "@s.whatsapp.net");
 
-                let isCreator = [...devss,...id_bot_N,...config.NUMERO_OWNER.split(",")].map((v) => v.replace(/[^0-9]/g) + "@s.whatsapp.net").includes(auteur_message);
+                let isCreator = [...devss,id_bot_N,...config.NUMERO_OWNER.split(",")].map((v) => v.replace(/[^0-9]/g) + "@s.whatsapp.net").includes(auteur_message);
                
                 if (!isCreator && config.MODE === 'prive'){ return }
                 if (ms.key && ms.key.remoteJid === 'status@broadcast' && config.LECTURE_AUTO_STATUS === "oui"){
